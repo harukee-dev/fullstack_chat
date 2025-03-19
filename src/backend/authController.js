dfaa676a0afd723e3e41c17f12ee38a3ee94eefc
harukee-dev <hdjemsmc@gmail.com>
2025-03-19 17:43:10 +0300

    added registration, hashing passwords and adding new registered users in mongodb

--- src/backend/authController.js
+++ src/backend/authController.js
@@ -1,8 +1,29 @@
+const User = require('./models/User')
+const Role = require('./models/Role')
+const bcrypt = require('bcryptjs')
+
 class authController {
   async registration(request, response) {
     try {
+      const { username, password } = request.body
+      const candidate = await User.findOne({ username })
+      if (candidate) {
+        return response
+          .status(400)
+          .json({ message: 'Пользователь с таким именем уже существует' })
+      }
+      const hashPassword = bcrypt.hashSync(password, 5)
+      const userRole = await Role.findOne({ value: 'USER' })
+      const user = new User({
+        username,
+        password: hashPassword,
+        roles: [userRole.value],
+      })
+      await user.save()
+      return response.json({ message: 'Пользователь успешно зарегистрирован' })
     } catch (e) {
       console.log(e)
+      response.status(400).json({ message: 'Registration error' })
     }
   }
 
@@ -10,6 +31,7 @@ class authController {
     try {
     } catch (e) {
       console.log(e)
+      response.status(400).json({ message: 'Login error' })
     }
   }
 
