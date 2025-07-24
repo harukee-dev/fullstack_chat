a999623799fb237f01f2bb1cde944d5a93eb599a
harukee-dev <hdjemsmc@gmail.com>
2025-07-24 03:28:59 +0300

    added chats slice

---
+++ src/slices/chatSlice.ts
@@ -0,0 +1,18 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+const initialState: any = {
+  chats: [],
+}
+
+const chatsSlice = createSlice({
+  name: 'chats',
+  initialState,
+  reducers: {
+    addChat: (state, action: PayloadAction<any>) => {
+      state.chats = [...state.chats, action.payload]
+    },
+    deleteChat: (state, action: PayloadAction<any>) => {
+      state.chats = state.chats.filter((el: any) => el.id !== action.payload)
+    },
+  },
+})
