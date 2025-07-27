88293151e49d596216a53d904955f84e8cac05e0
harukee-dev <hdjemsmc@gmail.com>
2025-07-27 21:38:07 +0300

    added online status for chats

--- src/slices/friendsSlice.ts
+++ src/slices/friendsSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 const initialState: any = {
   friends: [],
+  onlineFriends: [],
 }
 
 const friendsSlice = createSlice({
@@ -11,8 +12,15 @@ const friendsSlice = createSlice({
     setFriends: (state, action: PayloadAction<any>) => {
       state.friends = action.payload
     },
+    setOnlineFriends: (state, action: PayloadAction<any>) => {
+      state.onlineFriends = action.payload
+    },
+    addOnlineFriend: (state, action: PayloadAction<any>) => {
+      state.onlineFriends.push(action.payload)
+    },
   },
 })
 
-export const { setFriends } = friendsSlice.actions
+export const { setFriends, setOnlineFriends, addOnlineFriend } =
+  friendsSlice.actions
 export default friendsSlice.reducer
