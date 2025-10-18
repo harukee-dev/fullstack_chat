9b46ee055ce887118b45b3d8bcaac8a3a182ca48
harukee-dev <hdjemsmc@gmail.com>
2025-10-18 21:29:40 +0300

    fixed URLs

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = '185.207.64.7:10000'
+export const API_URL = 'http://185.207.64.7:10000'
 // export const API_URL = 'http://localhost:10000'
 export const TIP_URL = 'https://pay.cloudtips.ru/p/0020590b'
