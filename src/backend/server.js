6c486baf8a72e5ecf8fd6ef6ae853b93e772ecb4
harukee-dev <hdjemsmc@gmail.com>
2025-10-19 01:27:54 +0300

    fixed

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,12 +1,17 @@
 // Импорты и зависимости
 const express = require('express')
 const app = express()
-const http = require('http')
+const https = require('https')
+const fs = require('fs')
 const { Server } = require('socket.io')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const { secret } = require('./config')
-const server = http.createServer(app)
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/lynk.su/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/lynk.su/fullchain.pem'),
+}
+const server = https.createServer(options, app)
 const Message = require('./models/Message')
 const friendsRouter = require('./routes/friends')
 const path = require('path')
