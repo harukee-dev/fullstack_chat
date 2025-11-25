8e9ecd425a2dfe07bde247a6ebe23ff804e40262
harukee-dev <hdjemsmc@gmail.com>
2025-11-25 22:28:49 +0300

    redesigned register page

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -6,21 +6,21 @@ import { setToken } from '../../slices/authSlice'
 import { setUser } from '../../slices/currentUserSlice'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../../store'
-import { motion } from 'framer-motion'
+import lynkLogo from '../LoginPage/images/lynk-logo.png'
+import showPasswordIcon from '../LoginPage/images/show-password-icon.png'
 
 export const Register = () => {
   const [login, setLogin] = useState<string>('')
   const [password, setPassword] = useState<string>('')
-  const [confirmPassword, setConfirmPassword] = useState<string>('')
   const [error, setError] = useState<string>('')
   const [isVisiblePassword, setIsVisiblePassword] = useState<boolean>(false)
-  const [step, setStep] = useState<'info' | 'avatar'>('info')
+  const [step, setStep] = useState<'info' | 'avatar'>('avatar')
   const [avatar, setAvatar] = useState<string>(
-    'https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png'
+    // 'https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png'
+    'https://i.pinimg.com/736x/85/e8/fc/85e8fccc2983bdc283adbdaa6d761e25.jpg'
   )
   const navigate = useNavigate()
-  const isButtonHidden =
-    password !== '' && login !== '' && password === confirmPassword
+  const isButtonHidden = password !== '' && login !== ''
 
   const getPasswordStrength = (password: string) => {
     let strength: number = 0
@@ -38,7 +38,7 @@ export const Register = () => {
       case 1:
         return 'easy'
       case 2:
-        return 'easy'
+        return 'normal'
       case 3:
         return 'normal'
       case 4:
@@ -144,145 +144,86 @@ export const Register = () => {
   }
 
   const strength = getPasswordStrength(password)
-  const difficulty = getPasswordDifficulty(strength)
 
   return (
     <div className={cl.allPage}>
       {step === 'info' ? (
         <div className={cl.leftContainer}>
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4, delay: 0 }}
-          >
-            <h1 className={cl.welcomeText}>Welcome.</h1>
-            <Link className={cl.register} to={'/login'}>
-              Already have account? Sign in
-            </Link>
-          </motion.div>
-          <motion.input
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4, delay: 0.3 }}
-            className={cl.userInput}
-            type="text"
-            placeholder="login"
-            onBlur={(e) => setLogin(e.target.value)}
-          />
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4, delay: 0.5 }}
-            className={cl.passwordContainer}
-          >
+          <div className={cl.logoAndNameContainer}>
+            <img className={cl.lynkLogo} src={lynkLogo} alt="lynk" />
+            <h1 className={cl.lynkText}>Lynk</h1>
+          </div>
+          <div className={cl.navigationContainer}>
+            <button onClick={() => navigate('/login')} className={cl.loginLink}>
+              Sign In
+            </button>
+            <button className={cl.registerLink}>Sign Up</button>
+          </div>
+          <div className={cl.inputsContainer}>
             <input
-              className={cl.passwordInput}
-              type={isVisiblePassword ? 'text' : 'password'}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="password"
+              className={cl.userInput}
+              type="text"
+              placeholder="Enter Username"
+              onBlur={(e) => setLogin(e.target.value)}
             />
-            <p
-              className={
-                difficulty === 'easy'
-                  ? cl.easy
-                  : difficulty === 'normal'
-                  ? cl.normal
-                  : cl.hard
-              }
-            >
-              {difficulty}
-            </p>
-          </motion.div>
-          <div className={cl.passwordAndButtonDiv}>
-            <motion.input
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.4, delay: 0.7 }}
+            <input
               className={cl.userInput}
               type={isVisiblePassword ? 'text' : 'password'}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              placeholder="confirm password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter Password"
             />
-          </div>
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4, delay: 0.9 }}
-            className={cl.visibilityContainer}
-          >
-            <div
-              className={isVisiblePassword ? cl.checkboxChecked : cl.checkbox}
+
+            <button
               onClick={() => setIsVisiblePassword((v) => !v)}
-            />
-            <p className={cl.visibilityText}>show password</p>
-          </motion.div>
-          {error && <p className={cl.error}>{error}</p>}
-          <motion.button
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4, delay: 1.1 }}
-            disabled={!isButtonHidden}
-            onClick={handleRegister}
-            className={isButtonHidden ? cl.loginButton : cl.hiddenLoginButton}
-          >
-            Continue
-          </motion.button>
+              className={
+                isVisiblePassword
+                  ? cl.showPasswordButtonChecked
+                  : cl.showPasswordButton
+              }
+            >
+              <img
+                className={cl.showPasswordIcon}
+                src={showPasswordIcon}
+                alt="show-password"
+              />
+            </button>
+
+            {error && <p className={cl.error}>{error}</p>}
+            <button
+              disabled={!isButtonHidden}
+              onClick={handleRegister}
+              className={isButtonHidden ? cl.loginButton : cl.hiddenLoginButton}
+            >
+              Continue
+            </button>
+          </div>
         </div>
       ) : (
-        <div className={cl.leftContainerAvatar}>
-          <motion.h1
-            initial={{ opacity: 0, y: 15 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4, delay: 0 }}
-            className={cl.welcomeTextAvatar}
-          >
-            Set your avatar
-          </motion.h1>
-          <motion.img
-            initial={{ opacity: 0, y: 15 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4, delay: 0.4 }}
-            className={cl.userAvatar}
-            src={avatar}
-            alt="user-avatar"
-          />
-          <motion.input
-            initial={{ opacity: 0, y: 15 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4, delay: 0.6 }}
-            className={cl.userInputAvatar}
-            type="text"
-            placeholder="Enter image url"
-            onBlur={(e) =>
-              e.target.value === ''
-                ? setAvatar(
-                    'https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png'
-                  )
-                : setAvatar(e.target.value)
-            }
-          />
-          <motion.button
-            initial={{ opacity: 0, y: 15 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4, delay: 0.8 }}
-            onClick={changeAvatar}
-            className={cl.continueButtonAvatar}
-          >
-            {avatar ===
-            'https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png'
-              ? 'Later'
-              : 'Save'}
-          </motion.button>
+        <div className={cl.leftContainer}>
+          <h1 className={cl.lynkText}>Set avatar</h1>
+          <img className={cl.userAvatar} src={avatar} alt="user-avatar" />
+          <div className={cl.inputsContainer}>
+            <input
+              className={cl.userInput}
+              type="text"
+              placeholder="Enter image url"
+              onChange={(e) =>
+                e.target.value === ''
+                  ? setAvatar(
+                      'https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png'
+                    )
+                  : setAvatar(e.target.value)
+              }
+            />
+            <button onClick={changeAvatar} className={cl.loginButton}>
+              {avatar ===
+              'https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png'
+                ? 'Later'
+                : 'Save'}
+            </button>
+          </div>
         </div>
       )}
-      <div className={cl.rightContainer}>
-        <img
-          draggable={false}
-          className={cl.rightContainerImage}
-          src={''}
-          alt="background"
-        />
-      </div>
     </div>
   )
 }
